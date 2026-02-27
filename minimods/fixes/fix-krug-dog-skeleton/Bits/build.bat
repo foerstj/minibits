7c42479978e9541c12b9972ddf31bc7bd6439faa
Johannes Förstner <johannes@famfoerstner.de>
2026-02-27 21:18:19 +0100

    Modernize Fix Krug Dog Skeleton scripts & add to git

---
+++ minimods/fixes/fix-krug-dog-skeleton/Bits/build.bat
@@ -0,0 +1,29 @@
+:: name of map
+set map=minibits-demo-fix-krug-dog-skeleton
+:: name of map, case-sensitive
+set map_cs=Fix Krug Dog Skeleton
+
+:: path of Bits dir
+set bits=%~dp0.
+:: path of DS installation
+set ds=%DungeonSiege%
+:: path of TankCreator
+set tc=%TankCreator%
+
+set copyright=CC-BY-SA 2026
+set author=Johannes Förstner
+
+:: Compile map file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Compile main resource file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\contentdb" "%tmp%\Bits\world\contentdb" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Cleanup
+rmdir /S /Q "%tmp%\Bits"
