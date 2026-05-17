12e2bc60daa7af9d45b53f946a5e001641bc355a
Johannes Förstner <johannes@famfoerstner.de>
2026-05-17 13:50:39 +0200

    Clean up transform-into-gremal - it was turned into a full mod with a separate repo

--- minimods/transform-into-gremal/Bits/cleanup.bat
+++
@@ -1,9 +0,0 @@
-:: name of mod, case-sensitive
-set mod_cs=Transform Into Gremal
-
-:: path of DS installation
-set ds=%DungeonSiege%
-
-:: Cleanup resources so as not to confuse Siege Editor
-del "%ds%\DSLOA\%mod_cs%.dsres"
-del "%ds%\DSLOA\%mod_cs%.dsmap"
--- minimods/transform-into-gremal/README.md
+++
@@ -1,9 +0,0 @@
-# transform-into-gremal
-
-![pic](pic.jpg)
-
-Adds a Transform Into Gremal spell, for Nature Magic level 10. The Gremal will not be attacked.
-
-src: foerstj
-
-![pic](pic2.jpg)
